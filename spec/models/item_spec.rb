b58baa88f4673d10738da7513bfd09509167957f
abdyqew <aqudrat630@gmail,com>
2018-10-02 12:28:14 -0600

    adds invoice_items model/migration/spec relationships/validations

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -8,5 +8,6 @@ RSpec.describe Item, type: :model do
   end
   describe 'Relationships' do
     it {should belong_to(:merchant)}
+    it {should have_many(:invoices).through(:invoice_items)}
   end
 end
