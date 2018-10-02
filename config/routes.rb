ee60391f6f8a8cbec77068a9859babd19560fd7e
abdyqew <aqudrat630@gmail,com>
2018-10-02 16:08:07 -0600

    adds merchant find eps/spec/route

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
+      namespace :merchants do
+        get '/find_all', to: 'find#index'
+        get '/find', to: 'find#show'
+      end
       resources :merchants, only: [:index, :show]
     end
   end
