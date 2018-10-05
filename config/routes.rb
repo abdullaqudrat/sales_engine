b0cc98f9cfb802952edf8c76a8256a9db1802848
abdyqew <aqudrat630@gmail,com>
2018-10-05 17:34:56 -0600

    adds merchant random api/spec, ATP

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
       namespace :merchants do
         get '/find_all', to: 'find#index'
         get '/find', to: 'find#show'
+        get '/random', to: 'random#show'
         get '/most_revenue', to: 'most_revenue#index'
         get '/most_items', to: 'most_items#index'
         get '/revenue', to: 'revenue#index'
