a5c589a8af4e140c8026efc508068e5287b4f331
abdyqew <aqudrat630@gmail,com>
2018-10-06 14:33:32 -0600

    changes factories

--- spec/factories/invoice_items.rb
+++ spec/factories/invoice_items.rb
@@ -4,5 +4,7 @@ FactoryBot.define do
     unit_price { Faker::Commerce.price }
     item { nil }
     invoice { nil }
+    created_at { '2018-09-01 12:00:00 UTC'}
+    updated_at { '2018-09-01 12:00:00 UTC'}
   end
 end
--- spec/factories/transactions.rb
+++ spec/factories/transactions.rb
@@ -1,8 +1,8 @@
 FactoryBot.define do
   factory :transaction do
     credit_card_number { "#{rand(1000000000000000...9999999999999999)}" }
-    credit_card_expiration_date { "2018-10-02 10:57:57" }
+    credit_card_expiration_date { "2018-10-02" }
     result { "success" }
-    invoice { nil }
+    invoice { create(:invoice) }
   end
 end
