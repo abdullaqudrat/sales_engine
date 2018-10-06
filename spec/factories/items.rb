4445ee67d60bde7d0222ebc937c5a9175fae8887
abdyqew <aqudrat630@gmail,com>
2018-10-06 13:34:04 -0600

    adds item single finders spec/api

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -4,5 +4,7 @@ FactoryBot.define do
     description { Faker::Hacker.say_something_smart }
     unit_price { Faker::Commerce.price }
     merchant { create(:merchant) }
+    created_at { '2018-09-01 12:00:00 UTC'}
+    updated_at { '2018-09-01 12:00:00 UTC'}
   end
 end
--- spec/requests/api/v1/items/find_request_spec.rb
+++ spec/requests/api/v1/items/find_request_spec.rb
@@ -16,4 +16,45 @@ describe "Items API" do
       expect(item["id"]).to_not eq(i_3.id)
     end
   end
+  describe "find controller" do
+    it "should return items by name param" do
+      i_1, i_2, i_3 = create_list(:item, 3)
+
+      get "/api/v1/items/find?name=#{i_2.name}"
+
+      expect(response).to be_successful
+
+      item = JSON.parse(response.body)
+
+      expect(item["id"]).to eq(i_2.id)
+      expect(item["id"]).to_not eq(i_1.id)
+      expect(item["id"]).to_not eq(i_3.id)
+    end
+  end
+  describe "find controller" do
+    it "should return items by created_at param" do
+      i_1 = create(:item)
+
+      get "/api/v1/items/find?created_at=#{i_1.created_at}"
+
+      expect(response).to be_successful
+
+      item = JSON.parse(response.body)
+
+      expect(item["id"]).to eq(i_1.id)
+    end
+  end
+  describe "find controller" do
+    it "should return items by updated_at param" do
+      i_1 = create(:item)
+
+      get "/api/v1/items/find?updated_at=#{i_1.updated_at}"
+
+      expect(response).to be_successful
+
+      item = JSON.parse(response.body)
+
+      expect(item["id"]).to eq(i_1.id)
+    end
+  end
 end
