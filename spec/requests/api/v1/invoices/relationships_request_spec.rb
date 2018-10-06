e028f1ef8f6ade815881145cf8fdc61afda3a012
abdyqew <aqudrat630@gmail,com>
2018-10-06 16:46:38 -0600

    adds invoice - invoice_item rel endpoint spec/route/controller/api ATP

--- spec/requests/api/v1/invoices/relationships_request_spec.rb
+++ spec/requests/api/v1/invoices/relationships_request_spec.rb
@@ -15,4 +15,18 @@ describe "Invoice API" do
     expect(Transaction.all.count).to eq(6)
     expect(transactions.count).to eq(5)
   end
+  it "can send invoice_items associated with the invoice" do
+    invoice = create(:invoice)
+    create_list(:invoice_item, 5, invoice: invoice)
+    create(:invoice_item)
+
+    get "/api/v1/invoices/#{invoice.id}/invoice_items"
+
+    expect(response).to be_successful
+
+    invoice_items = JSON.parse(response.body)
+
+    expect(InvoiceItem.all.count).to eq(6)
+    expect(invoice_items.count).to eq(5)
+  end
 end
