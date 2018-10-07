9a8d3f46c6df417aae5987214ab0a7cca88e29a2
abdyqew <aqudrat630@gmail,com>
2018-10-07 16:46:44 -0600

    adds item most rev spec

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -10,4 +10,27 @@ RSpec.describe Item, type: :model do
     it {should belong_to(:merchant)}
     it {should have_many(:invoices).through(:invoice_items)}
   end
+  describe 'Class Methods' do
+    describe '::most_revenue' do
+      it 'should return x top ranked items by revenue' do
+        item_1, item_2, item_3 = create_list(:item, 3)
+        invoice_1, invoice_2, invoice_3 = create_list(:invoice, 3)
+        invoice_4 = create(:invoice, status: 'cancelled')
+        create(:transaction, invoice: invoice_1)
+        create(:transaction, invoice: invoice_2)
+        create(:transaction, invoice: invoice_3, result: "failed")
+        create(:transaction, invoice: invoice_3)
+        create(:transaction, invoice: invoice_4, result: "failed")
+        create(:invoice_item, item: item_1, invoice: invoice_2)
+        create(:invoice_item, item: item_1, invoice: invoice_3)
+        create(:invoice_item, item: item_2, invoice: invoice_4)
+        create(:invoice_item, item: item_2, invoice: invoice_4)
+        create(:invoice_item, item: item_3, invoice: invoice_1)
+        create(:invoice_item, item: item_3, invoice: invoice_1)
+
+        expect(Item.most_revenue).to eq([item_3, item_1, item_2])
+        expect(Item.most_revenue(2)).to eq([item_3, item_1])
+      end
+    end
+  end
 end
